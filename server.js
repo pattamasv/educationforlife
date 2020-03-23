052cd34f89bd703ae097c76ddf739e8d64eb769d
Pattamas Wiratchanee <c61156@outlook.com>
2020-03-23 15:54:05 +0700

    18 : update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var fs = require('fs');
-fs.readFile('./home.html', function(err, html){
+
+
+fs.readFile('./yuva.html', function(err, html){
     http.createServer(function(request, response){
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
